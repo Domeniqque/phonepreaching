4d99c290326d21256a9bc6cfb0fb1073e534b0c6
Domeniqque Dylluar <domeniqque@hotmail.com>
2020-08-08 06:11:15 -0400

    fix

--- pages/privacy/index.js
+++ pages/privacy/index.js
@@ -10,7 +10,7 @@ function Privacy() {
         <p>We don't need and don't want to store your data. In fact, we don't even have servers to store your data 😉. They are yours and we will never ask for them. What happens on your phone, remains on your phone!</p>
 
         <p>
-          However, only once will the app send us some information about you: when an <strong> error in the app </strong> acontecer happens (believe me, we don't want to see your data). To help us identify and correct
+          However, only once will the app send us some information about you: when an <strong> error in the app </strong> happens (believe me, we don't want to see your data). To help us identify and correct
           the error we need some basic information like: your <strong> country </strong>, your phone's <strong> model </strong>, the <strong> error log </strong> and some other information collected by <a rel="noreferrer" href="https://policies.google.com/privacy?hl=en-US"> Google Analytcs</a>.
           This information is stored on Google's servers. After identifying and addressing the error, we delete the information. Don't worry, none of this data is enough to identify you. But if you still have any questions please contact: <a href="mailto:dylluar-labs@outlook.com"> dylluar-labs@outlook.com</a>.
         </p>
